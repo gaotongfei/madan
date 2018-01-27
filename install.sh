c35ba1e139dc581f402d8b9c7ceadb4f7bfa41b7
Taff Gao <gaotongfei1995@gmail.com>
2018-01-27 17:12:53 +0800

    update install bash script

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 gem uninstall madan
 gem uninstall tty-markdown
 rm -rf /tmp/tty-markdown
-echo -e '\ncloning tty-markdown...\n'
+echo $'\ncloning tty-markdown...\n'
 git clone git@github.com:piotrmurach/tty-markdown.git /tmp/tty-markdown
 cd /tmp/tty-markdown
 git checkout 790106a
 rake build
-echo -e '\ninstalling tty-markdown...\n'
+echo $'\ninstalling tty-markdown...\n'
 gem install pkg/tty-markdown-0.1.0.gem
-echo -e '\ninstalling madan...\n'
+echo $'\ninstalling madan...\n'
 gem install madan
